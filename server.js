7b686fb8b856c35607b0d1e08ba34b1b8080349a
Zach Hudson <hudsonzachpro@gmail.com>
2022-08-04 10:30:13 -0600

    edited max_timeout variable to attempt to rectify H13 error

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3000;
 const sql = postgres(
   `postgres://localhost:5432/tracker`,
   process.env.NODE_ENV === "production"
-    ? postgres({ ssl: { rejectUnauthorized: false }, connectionString: process.env.DATABASE_URL })
+    ? postgres({
+        ssl: { rejectUnauthorized: false },
+        connectionString: process.env.DATABASE_URL,
+        max_lifetime: 60 * 30,
+      })
     : postgres(`postgres://localhost:5432/tracker`)
 );
 
